e1e811afee6c43e17dd7a80d6d97c74fabfae36f
Steve Walsh <swalsh@rainbow.phys.uconn.edu>
2015-02-10 21:04:35 -0500

    considering n dimensional matrix class, playing with ifstream

--- main.cpp
+++ main.cpp
@@ -1,9 +1,46 @@
 #include <iostream>
 #include <cstdlib>
 #include <stdio.h>
+#include <fstream>
+#include <cmath>
+#include <vector>
 using namespace std;
 
-int main(int argc, char *argv[]) 
-{
+class Matrix{
 
+  std::vector<double> mElements; 
+
+public:
+  Matrix(std::vector<double> elements):
+    mElements(elements){}
+
+
+};
+
+
+
+
+
+int main(int argc, char *argv[]) {
+ 
+
+  ifstream enuc;
+  enuc.open("enuc.dat");
+  char data[100];
+
+  enuc >> data;
+
+  cout << data << endl;
+
+  std::vector<double> vecta;
+
+  vecta.push_back(1.0);
+  vecta.push_back(2.0);
+  vecta.push_back(3.0);
+  vecta.push_back(4.0);
+
+  Matrix a = Matrix(vecta);
+  int sup = 1;
 }
+
+
