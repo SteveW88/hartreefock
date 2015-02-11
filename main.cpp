3bbe67687043fb9f334e82cb9cf7dfd0e132709e
Steve Walsh <swalsh@rainbow.phys.uconn.edu>
2015-02-11 13:33:41 -0500

    overlap to upper triangular, determine eigenvalues

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int main(int argc, char *argv[]) {
   
   // Array Indexing
-  int BIGNUM = 100;
+  int BIGNUM = 10000;
   int ioff[BIGNUM];
   ioff[0] = 0;
   for(int i = 1; i < BIGNUM; i++)
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]) {
   enucFile >> enuc;
   enucFile.close();
  
-
+  
   //Read overlap
   FILE *input;
   double val;
@@ -78,7 +78,7 @@ int main(int argc, char *argv[]) {
   }
   fclose(input);
 
-  //Core Hamiltonia
+  //Core Hamiltonian
   double coreH[7][7];
   for(int i = 0; i < 7; i++){
     for(int j = 0; j < 7; j++){
@@ -87,8 +87,67 @@ int main(int argc, char *argv[]) {
   }
   
  
+  //Read two-electron integrals
+  double tei[BIGNUM];
+  input = fopen("eri.dat", "r");
+  while(fscanf(input, "%d %d %d %d %lf", &i, &j, &k, &l, &val) != EOF){
+    i -= 1;
+    j -= 1;
+    k -= 1;
+    l -= 1;
+    
+    ij = (i>j) ? ioff[i] + j : ioff[j] + i;
+    kl = (k>l) ? ioff[k] + l : ioff[k] + l;
+    ijkl = (ij > kl) ? ioff[ij] + kl : ioff[kl] + ij;
+    tei[ijkl] = val;
   
-  
+  }
+  fclose(input);
+ 
+  ///////////////////////
+  //Diagonalize overlap
+  /////////////////////
+ 
+  //Upper Triangluar
+  int numRows = 7;
+  for(int i = 0; i < numRows; i++){
+    //determine largest row
+    int largestRow = i;
+    for(int k = i; k < numRows; k++){
+      if(abs(overlap2[k][i]) > abs(overlap2[i][k])){
+        largestRow = k;
+      }
+    }
+
+    if(largestRow != i){
+      //rowswap
+      int columns = numRows - i;
+      double temp[1][columns];
+      for(int col = i; col < numRows; col++){
+        temp[0][col] = overlap2[i][col];
+        overlap2[i][col] = overlap2[largestRow][col];
+        overlap2[largestRow][col] = temp[0][col];
+      }
+    }
+    // row reduce
+    if(overlap2[i][i] != 0){
+       
+      for(int j = 1; j < numRows;  j++){
+        double factor = overlap2[i+j][i]/overlap2[i][i];
+        for(int col = i; col < numRows - i; col++){
+          
+          overlap2[i+j][col] -= (overlap2[i][col]*factor);
+        }
+      }
+    }
+  }
+
+    // Determinant = sum of diagonal elements
+  double eigenValues[numRows];
+  for(int i = 0; i < numRows; i++){
+    eigenValues[i] = overlap2[i][i];
+  }
+
 }
 
 
