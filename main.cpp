acabb306863dc14b9b98102cc9842d5a5abcc759
Steve Walsh <swalsh@rainbow.phys.uconn.edu>
2015-03-30 22:58:15 -0400

    added 'define dim #' to main.cpp as well as changed values within for a less static program. also corrected iteration process (now properly iterates but the value is inconsistent with expected value.

--- main.cpp
+++ main.cpp
@@ -7,18 +7,18 @@
 #include <gsl/gsl_eigen.h>
 #include "matrices.hpp"
 
-
+#define dim 7
 
 void Diagonalize(double eigenval[], double eigenvec[], double origMat[]){  //Set for 7X7 matrices
-
+ 
   gsl_matrix_view m 
-    = gsl_matrix_view_array (origMat, 7, 7);
+    = gsl_matrix_view_array (origMat, dim, dim);
 
-  gsl_vector_complex *eval = gsl_vector_complex_alloc (7);
-  gsl_matrix_complex *evec = gsl_matrix_complex_alloc (7, 7);
+  gsl_vector_complex *eval = gsl_vector_complex_alloc (dim);
+  gsl_matrix_complex *evec = gsl_matrix_complex_alloc (dim, dim);
   
   gsl_eigen_nonsymmv_workspace * w = 
-    gsl_eigen_nonsymmv_alloc (7);
+    gsl_eigen_nonsymmv_alloc (dim);
   
   gsl_eigen_nonsymmv (&m.matrix, eval, evec, w);
   
@@ -30,7 +30,7 @@ void Diagonalize(double eigenval[], double eigenvec[], double origMat[]){  //Set
   {
     int i, j;
     
-    for (i = 0; i < 7; i++)
+    for (i = 0; i < dim; i++)
       {
         gsl_complex eval_i 
           = gsl_vector_complex_get (eval, i);
@@ -39,12 +39,12 @@ void Diagonalize(double eigenval[], double eigenvec[], double origMat[]){  //Set
           
         eigenval[i] = GSL_REAL(eval_i);
         
-        for (j = 0; j < 7; ++j)
+        for (j = 0; j < dim; ++j)
           {
             gsl_complex z = 
               gsl_vector_complex_get(&evec_i.vector, j);
            
-            eigenvec[(i*7)+j] = GSL_REAL(z);
+            eigenvec[(i*dim)+j] = GSL_REAL(z);
           }
       }
   }
@@ -83,27 +83,27 @@ int main(int argc, char *argv[]) {
   //
   FILE *input;
   double val;
-  double overlap[49];
-  double overlap2[7][7];
+  double overlap[dim*dim];
+  // double overlap2[7][7];
   input = fopen("s.dat", "r");
   while(fscanf(input, "%d %d %lf", &i, &j, &val) != EOF){
     i -= 1;
     j -= 1;
-    overlap[(i*7)+j] = val;
+    overlap[(i*dim)+j] = val;
     if(i != j)
-      overlap[(j*7)+i] = val;
+      overlap[(j*dim)+i] = val;
     
 
-    overlap2[i][j] = val;
-    if (i != j)
-      overlap2[j][i] = val;
+    //overlap2[i][j] = val;
+    //if (i != j)
+    // overlap2[j][i] = val;
   }
   fclose(input);
 
   //Read kinetic energy
   //
-  double ke[33];
-  double ke2[7][7];
+  double ke[(dim*dim-dim)/2 + dim];
+  double ke2[dim][dim];
   input = fopen("t.dat", "r");
   while(fscanf(input, "%d %d %lf", &i, &j, &val) != EOF){
     i -= 1;
@@ -120,7 +120,7 @@ int main(int argc, char *argv[]) {
   //Read nuclear-attraction integrals
   //
   double nai[BIGNUM];
-  double nai2[7][7];
+  double nai2[dim][dim];
   input = fopen("v.dat", "r");
   while(fscanf(input, "%d %d %lf", &i, &j, &val) != EOF){
     i -= 1;
@@ -135,12 +135,12 @@ int main(int argc, char *argv[]) {
 
   //Core Hamiltonian
   //
-  double coreH[7][7];
-  double coreH2[49];
-  for(int i = 0; i < 7; i++){
-    for(int j = 0; j < 7; j++){
+  double coreH[dim][dim];
+  double coreH2[dim*dim];
+  for(int i = 0; i < dim; i++){
+    for(int j = 0; j < dim; j++){
       coreH[i][j] = ke2[i][j] + nai2[i][j];
-      coreH2[(i*7)+j] = ke2[i][j] + nai2[i][j];
+      coreH2[(i*dim)+j] = ke2[i][j] + nai2[i][j];
     }
   }
   
@@ -167,13 +167,13 @@ int main(int argc, char *argv[]) {
   //Step 4: Diagonalize overlap
   ///////////////////////
   
-  double eigenvalues[7];
-  double eigenvectors[49];
+  double eigenvalues[dim];
+  double eigenvectors[dim*dim];
  
   Diagonalize(eigenvalues,eigenvectors,overlap);
-  Matrix Hcore = Matrix(coreH2,7);  // Correct
-  DiagMat As = DiagMat(eigenvalues,7);
-  Matrix Ls = Matrix(eigenvectors,7).T();
+  Matrix Hcore = Matrix(coreH2,dim);  // Correct
+  DiagMat As = DiagMat(eigenvalues,dim);
+  Matrix Ls = Matrix(eigenvectors,dim).T();
   Matrix LsT = Ls.T();
   Matrix Sso = ((Ls*As.ToPower(-0.5))*Ls.T()); // Correct
   
@@ -183,25 +183,25 @@ int main(int argc, char *argv[]) {
   Matrix Fockinit = (Sso.T() * Hcore * Sso); // Correct
 
   double * Fockinitarray = Fockinit.ToArraySTATIC();
-  double Farray[49];                        // Correct
-  for(int i=0; i < 49; i++){
+  double Farray[dim*dim];                        // Correct
+  for(int i=0; i < (dim*dim); i++){
     Farray[i] = *(Fockinitarray+i);
   }
   Diagonalize(eigenvalues,eigenvectors,Farray);
 
 
 
-  Matrix C0prime = Matrix(eigenvectors,7).T();
-  DiagMat eps0 = DiagMat(eigenvalues,7);
+  Matrix C0prime = Matrix(eigenvectors,dim).T();
+  DiagMat eps0 = DiagMat(eigenvalues,dim);
   Matrix C0 = (Sso * C0prime);  // C0 values match in value, not in sign
 
 
 
   // build density matrix
-  Matrix DensInit = Matrix(7);
-  for(int k=0; k < 7; k++){
-    for(int j=0; j < 7; j++){
-      for(int i=0; i <4; i++){ // Set to 4 works (sum from 0 to N/2)
+  Matrix DensInit = Matrix(dim);
+  for(int k=0; k < dim; k++){
+    for(int j=0; j < dim; j++){
+      for(int i=0; i <((dim+1)*0.5); i++){ // Set to 4 works (sum from 0 to N/2)
         DensInit(k,j) += C0(k,i) * C0(j,i);   // Correct
       }
     }
@@ -210,87 +210,91 @@ int main(int argc, char *argv[]) {
   // Step 6: Compute Initial SCF Energy
   //
   double Eelec = 0;
-  for(int i=0; i<7; i++){
-    for(int j=0; j<7; j++){
+  for(int i=0; i<dim; i++){
+    for(int j=0; j<dim; j++){
       Eelec +=  DensInit(i,j) * (Hcore(i,j) + Fockinit(i,j));
     }
   }
- 
   double Etot = Eelec + enuc;  //incorret
   double Etotprev;
-  
+  std::cerr << Etot << std::endl;
 
-  // Step 7: Compute New Fock Matrix
+  // Step 7 Compute New Fock Matrix
   //
+
+  Matrix Densprev = DensInit;
+ 
   do{
     Etotprev = Etot;
 #define INDEX(i,j) (i>j) ? (ioff[i]+j) : (ioff[j]+i);
-  double F[7][7];
-  for(int i=0; i < 7; i++)
-    for(int j=0; j < 7; j++) {
-      F[i][j] = coreH[i][j];
-      for(int k=0; k < 7; k++)
-        for(int l=0; l < 7; l++) {
-          ij = INDEX(i,j);
-          kl = INDEX(k,l);
-          ijkl = INDEX(ij,kl);
-          ik = INDEX(i,k);
-          jl = INDEX(j,l);
-          ikjl = INDEX(ik,jl);
- 
-          F[i][j] += DensInit(k,l) * (2.0 * tei[ijkl] - tei[ikjl]);
-        }
-    }
-  double F1[49];
-  for(int i=0; i < 7; i++){
-    for(int j=0; j<7; j++){
-      F1[(i*7)+j] = F[i][j];
-    }
-  }
-  Matrix newFock = Matrix(F1,7);
-  // Step 8: Build New Density Matrix
-  //
-  Matrix newFockinit = (Sso.T() * newFock * Sso); 
-
-  double * newFockinitarray = newFockinit.ToArraySTATIC();
-  double newFarray[49];                       
-  for(int i=0; i < 49; i++){
-    newFarray[i] = *(newFockinitarray+i);
-  }
-  Diagonalize(eigenvalues,eigenvectors,newFarray);
-     
-  Matrix newC0prime = Matrix(eigenvectors,7).T();
-  DiagMat neweps0 = DiagMat(eigenvalues,7);
-  Matrix newC0 = (Sso * C0prime);  
-
-  // build density matrix
-  Matrix newDensInit = Matrix(7);
-  for(int k=0; k < 7; k++){
-    for(int j=0; j < 7; j++){
-      for(int i=0; i <4; i++){ 
-        newDensInit(k,j) += newC0(k,i) * newC0(j,i);    
+    double F[dim][dim];
+    for(int i=0; i < dim; i++)
+      for(int j=0; j < dim; j++) {
+        F[i][j] = coreH[i][j];
+        for(int k=0; k < dim; k++)
+          for(int l=0; l < dim; l++) {
+            ij = INDEX(i,j);
+            kl = INDEX(k,l);
+            ijkl = INDEX(ij,kl);
+            ik = INDEX(i,k);
+            jl = INDEX(j,l);
+            ikjl = INDEX(ik,jl);
+            
+            F[i][j] += Densprev(k,l) * (2.0 * tei[ijkl] - tei[ikjl]);
+          }
+      }
+    double F1[dim*dim];
+    for(int i=0; i < dim; i++){
+      for(int j=0; j<dim; j++){
+      F1[(i*dim)+j] = F[i][j];
       }
     }
-  }
-
-
-  // Step 9: Compute New SCF Energy
-  //
-  double Eelec1 = 0;
-  for(int i=0; i<7; i++){
-    for(int j=0; j<7; j++){
-      Eelec1 +=  newDensInit(i,j) * (Hcore(i,j) + newFockinit(i,j));
+    Matrix newFock = Matrix(F1,dim);
+    // Step 8: Build New Density Matrix
+    //
+    Matrix newFockinit = (Sso.T() * newFock * Sso); 
+    
+    double * newFockinitarray = newFockinit.ToArraySTATIC();
+    double newFarray[dim*dim];                       
+    for(int i=0; i < (dim*dim); i++){
+      newFarray[i] = *(newFockinitarray+i);
     }
-  }
-  
-  Etot = Eelec1 + enuc;  //incorret
- 
+    Diagonalize(eigenvalues,eigenvectors,newFarray);
+    
+    Matrix newC0prime = Matrix(eigenvectors,dim).T();
+    DiagMat neweps0 = DiagMat(eigenvalues,dim);
+    Matrix newC0 = (Sso * newC0prime);  
+    
+    // build density matrix
+    Matrix newDensInit = Matrix(dim);
+    for(int k=0; k < dim; k++){
+      for(int j=0; j < dim; j++){
+        for(int i=0; i <((dim+1)*0.5); i++){ 
+          newDensInit(k,j) += newC0(k,i) * newC0(j,i);    
+        }
+      }
+    }
+    
+    
+    // Step 9: Compute New SCF Energy
+    //
+    double Eelec1 = 0;
+    for(int i=0; i<dim; i++){
+      for(int j=0; j<dim; j++){
+        Eelec1 += newDensInit(i,j) * (Hcore(i,j) + newFockinit(i,j));
+      }
+    }
+    
+    Etot = Eelec1 + enuc;  //incorret
+    std::cerr << "Etot: " << Etot << " "
+              << "Eprev: " << Etotprev << std::endl;
+    Densprev = newDensInit;
   }while(fabs(Etot - Etotprev)> 1e-6);  // Step 10: Test for Convergence
-
-
+  
+  
   printf("Energy Value: %f \n", Etot);
-
- return 0;
+  
+  return 0;
 
 }
 
